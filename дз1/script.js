f16e16506c815f063ae0e4f046ff9942b8300d39
Magggri <78700625+Magggri@users.noreply.github.com>
2021-02-07 21:52:19 +0300

    Add files via upload

---
+++ дз1/script.js
@@ -0,0 +1,8 @@
+document.querySelector('.song').addEventListener('click',function() {
+window.location.href = "https://www.youtube.com/watch?v=kk3_5AHEZxE";
+});
+
+document.querySelector('.tema').addEventListener('click',function() {
+this.classList.toggle("night");
+});
+
